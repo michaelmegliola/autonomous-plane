4fd1d0787239e201c15c35d1c650949812e59fe1
michaelmegliola <michael.megliola@gmail.com>
2017-10-05 06:56:24 -0400

    initial test files

---
+++ test_9dof/test_9dof.ino
@@ -0,0 +1,93 @@
+#include <Wire.h>
+#include <Adafruit_FXAS21002C.h>
+#include <Adafruit_FXOS8700.h>
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_FXAS21002C gyro = Adafruit_FXAS21002C(0x0021002C);
+Adafruit_FXOS8700 accelmag = Adafruit_FXOS8700(0x8700A, 0x8700B);
+
+void displaySensorDetails(void)
+{
+  sensor_t gyro_sensor;
+  gyro.getSensor(&gyro_sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(gyro_sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(gyro_sensor.version);
+  Serial.print  ("Unique ID:    0x"); Serial.println(gyro_sensor.sensor_id, HEX);
+  Serial.print  ("Max Value:    "); Serial.print(gyro_sensor.max_value); Serial.println(" rad/s");
+  Serial.print  ("Min Value:    "); Serial.print(gyro_sensor.min_value); Serial.println(" rad/s");
+  Serial.print  ("Resolution:   "); Serial.print(gyro_sensor.resolution); Serial.println(" rad/s");
+  Serial.println("------------------------------------");
+  Serial.println("");
+    
+  sensor_t accel, mag;
+  accelmag.getSensor(&accel, &mag);
+  Serial.println("------------------------------------");
+  Serial.println("ACCELEROMETER");
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(accel.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(accel.version);
+  Serial.print  ("Unique ID:    0x"); Serial.println(accel.sensor_id, HEX);
+  Serial.print  ("Min Delay:    "); Serial.print(accel.min_delay); Serial.println(" s");
+  Serial.print  ("Max Value:    "); Serial.print(accel.max_value, 4); Serial.println(" m/s^2");
+  Serial.print  ("Min Value:    "); Serial.print(accel.min_value, 4); Serial.println(" m/s^2");
+  Serial.print  ("Resolution:   "); Serial.print(accel.resolution, 8); Serial.println(" m/s^2");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  Serial.println("------------------------------------");
+  Serial.println("MAGNETOMETER");
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(mag.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(mag.version);
+  Serial.print  ("Unique ID:    0x"); Serial.println(mag.sensor_id, HEX);
+  Serial.print  ("Min Delay:    "); Serial.print(accel.min_delay); Serial.println(" s");
+  Serial.print  ("Max Value:    "); Serial.print(mag.max_value); Serial.println(" uT");
+  Serial.print  ("Min Value:    "); Serial.print(mag.min_value); Serial.println(" uT");
+  Serial.print  ("Resolution:   "); Serial.print(mag.resolution); Serial.println(" uT");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(2500);
+}
+
+void setup(void)
+{
+  Serial.begin(9600);
+
+  /* Wait for the Serial Monitor */
+  while (!Serial) {
+    delay(1);
+  }
+  
+  /* Initialise the sensor */
+  if(!gyro.begin())
+  {
+    /* There was a problem detecting the FXAS21002C ... check your connections */
+    Serial.println("Ooops, no FXAS21002C detected ... Check your wiring!");
+    while(1);
+  }
+
+  /* Initialise the sensor */
+  if(!accelmag.begin(ACCEL_RANGE_4G))
+  {
+    /* There was a problem detecting the FXOS8700 ... check your connections */
+    Serial.println("Ooops, no FXOS8700 detected ... Check your wiring!");
+    while(1);
+  }
+
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+}
+
+void loop(void)
+{
+  /* Get a new sensor event */
+  sensors_event_t event;
+  gyro.getEvent(&event);
+
+  /* Display the results (speed is measured in rad/s) */
+  Serial.print("X: "); Serial.print(event.gyro.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.gyro.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.gyro.z); Serial.print("  ");
+  Serial.println("rad/s ");
+  delay(500);
+}
